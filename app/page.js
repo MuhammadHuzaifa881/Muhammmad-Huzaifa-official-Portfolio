903d8d0c7ac380be9175871aa0f7c6fab1d65c5c
MuhammadHuzaifa881 <huzaifanasir258@gmail.com>
2025-08-26 11:41:10 +0500

    experience

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 // import { personalData } from "@/utils/data/personal-data";
 // import Education from "./components/homepage/education";
-// import Experience from "./components/homepage/experience";
+import Experience from "./components/homepage/experience";
 // import Projects from "./components/homepage/projects";
 // import Skills from "./components/homepage/skills";
 
@@ -14,11 +14,11 @@ export default async function Home() {
     <div suppressHydrationWarning >
       <HeroSection />
       <AboutSection />
-      {/* <Experience /> */}
-      <Skills />
-      <Projects />
-      <Education />
-      <ContactSection />
+      <Experience />
+      {/* <Skills /> */}
+      {/* <Projects /> */}
+      {/* <Education /> */}
+      {/* <ContactSection /> */}
     </div>
   )
 };
