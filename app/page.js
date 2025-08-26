eb96b02dccd5c827fbe6a0f0db9a7fb2311a07ac
MuhammadHuzaifa881 <huzaifanasir258@gmail.com>
2025-08-26 11:44:04 +0500

    skills

--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 // import { personalData } from "@/utils/data/personal-data";
 // import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
+// import Experience from "./components/homepage/experience";
 // import Projects from "./components/homepage/projects";
-// import Skills from "./components/homepage/skills";
+import Skills from "./components/homepage/skills";
 
 export default async function Home() {
 
