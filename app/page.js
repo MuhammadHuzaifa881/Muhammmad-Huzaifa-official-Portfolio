cf3c673e436e96466d3fd8e5c8e0a8550f84b552
MuhammadHuzaifa881 <huzaifanasir258@gmail.com>
2025-08-26 14:04:47 +0500

    skills

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client"
 import HeroSection from "./components/homepage/hero-section";
 import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
+// import ContactSection from "./components/homepage/contact";
 // import { personalData } from "@/utils/data/personal-data";
 import Education from "./components/homepage/education";
 // import Experience from "./components/homepage/experience";
@@ -18,7 +18,7 @@ export default async function Home() {
       <Skills />
       <Projects />
       <Education />
-      <ContactSection />
+      {/* <ContactSection /> */}
     </div>
   )
 };
