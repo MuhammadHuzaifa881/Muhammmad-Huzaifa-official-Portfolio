b33a7e9a19e695ba86a4997d6478b948b61c43fd
MuhammadHuzaifa881 <huzaifanasir258@gmail.com>
2025-08-26 14:37:12 +0500

    updated code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        Green: "#1fdf64",
+        Black: "#000",
+        DeepNightBlack: "#121212",
+        MidNightBlack: "#181818",
+        EveningBlack: "#1a1a1a",
+        DarkGray: "#282828",
+        SlateGray: "#404040",
+        LightGray: "#959595",
+        SilverGray: "#B3B3B3",
+        Snow: "#ffffff",
       },
-      container: {
-        center: true,
-        padding: {
-          DEFAULT: "1rem",
-          sm: "2rem",
-          lg: "3rem",
-          xl: "4rem",
-          "2xl": "4rem",
-          "3xl": "5rem",
-        },
-      },
+      fontFamily: {
+        'cascadia-normal': ['cascadia-normal'],
+        'circular': ['circular-normal', 'sans-serif'],
+        'circular-light': ['circular-light', 'sans-serif'],
+        'circular-normal': ['circular-normal', 'sans-serif'],
+        'circular-medium': ['circular-medium', 'sans-serif'],
+        'circular-bold': ['circular-bold', 'sans-serif'],
 
-      extend: {
-        screens: {
-          "4k": "1980px",
-        },
-      },
+      }
     },
   },
-  plugins: [],
 }
