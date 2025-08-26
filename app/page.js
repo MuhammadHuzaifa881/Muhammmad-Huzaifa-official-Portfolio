8e853eea017e7b253152e025a0c30a2d12a37d5f
MuhammadHuzaifa881 <huzaifanasir258@gmail.com>
2025-08-26 05:07:33 +0500

    use effect removed

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 // import { personalData } from "@/utils/data/personal-data";
 // import Education from "./components/homepage/education";
-// import Experience from "./components/homepage/experience";
+import Experience from "./components/homepage/experience";
 // import Projects from "./components/homepage/projects";
 // import Skills from "./components/homepage/skills";
 
@@ -12,11 +12,10 @@ export default async function Home() {
 
   return (
     <div suppressHydrationWarning >
-      hello
       <HeroSection />
       <AboutSection />
-      {/* <Experience /> */}
-      {/* <Skills /> */}
+      <Experience />
+      <Skills />
       {/* <Projects /> */}
       {/* <Education /> */}
       <ContactSection />
